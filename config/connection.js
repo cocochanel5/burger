36bc39df81f7fa14d2924e9f8022d7320f0c42fe
Chanel Farrell <chanel.farrell3@gmail.com>
2019-09-22 12:52:00 -0700

    trying more jawdb fixes

--- config/connection.js
+++ config/connection.js
@@ -1,66 +1,32 @@
 var mysql = require("mysql");    
-var connection;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: 'localhost'
-    user: 'root'
-    password: 'hacktheplanet',
-    database: 'todoagain_db'
-  });
-};
+var source = {
+	
+	localhost: {
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: '',
+		database: 'burger_db'
+	},
+	jawsDB: {
+		port: 3306,
+		host: 'g9fej9rujq0yt0cd.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+		user: 'n25h6tz1xxz6uvu6',
+		password: 'wkwl1hv3s25qrc8i',
+		database: 'dq5m9m2ysrfki6hy'
+	}
 
-// connection = mysql.createConnection({ 
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "",
-//     database: "burger_db"
-// });
+}
 
+var connection = mysql.createConnection( source.jawsDB );
 
-connection.connect(function(err) {     
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+connection.connect(function (err) {
+	if (err) {
+		console.error('error connecting: ' + err.stack);
+		return;
+	}
+	console.log('Databased connected as id: ' + connection.threadId);
 });
 
-module.exports = connection;   
-
-
-// var mysql = require('mysql');
-
-// var source = {
-	
-// 	localhost: {
-// 		port: 3306,
-// 		host: 'localhost',
-// 		user: 'root',
-// 		password: '',
-// 		database: 'burger_db'
-// 	},
-// 	jawsDB: {
-// 		port: 3306,
-// 		host: 'gx97kbnhgjzh3efb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-// 		user: 'vt0eotyupp1i15ni',
-// 		password: 'qz6dygedxs71a8ou',
-// 		database: 'u4wogneutc6rikrq'
-// 	}
-
-// }
-
-// var connection = mysql.createConnection( source.jawsDB );
-
-// connection.connect(function (err) {
-// 	if (err) {
-// 		console.error('error connecting: ' + err.stack);
-// 		return;
-// 	}
-// 	console.log('Databased connected as id: ' + connection.threadId);
-// });
-
-// module.exports = connection;
+module.exports = connection;
